7bdc632d406430a0cb898f791adb2ae9e35f17bf
Ethan Sherr <esherrthan@gmail.com>
2019-03-21 09:57:22 -0500

    reorg 2

---
+++ Source/PlayerVs/Actors/Magazine.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Grippables/GrippableActor.h"
+#include "Magazine.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PLAYERVS_API AMagazine : public AGrippableActor
+{
+	GENERATED_BODY()
+	
+};
